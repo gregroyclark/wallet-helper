d4f3380c5eedcb5c3e3a12b1eab9f1e4ac387f7a
gregroyclark <gregroyclark@gmail.com>
2021-05-22 23:13:01 -0500

    added Cashflow component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Header } from './components/Header.js'
 import { Balance } from './components/Balance.js'
+import { Cashflow } from './components/Cashflow.js'
 
 import './App.css';
 
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <div className="container">
         <Balance />
+        <Cashflow />
       </div>
     </div>
   );
