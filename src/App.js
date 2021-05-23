0cfd52f95d180616cbf9ae1c8e32d236db950014
gregroyclark <gregroyclark@gmail.com>
2021-05-22 23:05:38 -0500

    added Balance

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from './components/Header.js'
+import { Balance } from './components/Balance.js'
 
 import './App.css';
 
@@ -7,6 +8,9 @@ function App() {
   return (
     <div>
       <Header />
+      <div className="container">
+        <Balance />
+      </div>
     </div>
   );
 }
