b49fb59d27fb3437f4262fdecc8d09dcd4e95238
gregroyclark <gregroyclark@gmail.com>
2021-05-23 10:36:50 -0500

    added state, added dynamic income & expenses

--- src/components/Cashflow.js
+++ src/components/Cashflow.js
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 export const Cashflow = () => {
+    const { transactions } = useContext(GlobalContext);
+
+    const amounts = transactions.map(transaction => transaction.amount)
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => (acc += item), 0)
+        .toFixed(2);
+
+        const expense = (
+            amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1
+        ).toFixed(2);
+
     return (
         <div className="cashflow-container">
             <div>
                 <h4>Income</h4>
-                <p className="money plus">+$0.00</p>
+                <p className="money plus">{income}</p>
             </div>
             <div>
                 <h4>Expenses</h4>
-                <p className="money minus">-$0.00</p>
+                <p className="money minus">{expense}</p>
             </div>
         </div>
     )
